4e94161b9825ab1184fe6af798620aa5d0455645
Paloma Arce <palomaarces@outlook.com>
2021-04-24 16:00:54 -0300

    manipulando elementos em html

--- javascript/js/main.js
+++ javascript/js/main.js
@@ -1,24 +1,43 @@
-function soma(n1, n2){
-    return n1+ n2;
+function botao(){
+    //alert("Obrigada por clicar");
+    var element = document.getElementById("btnAgradecimento").innerHTML = "<strong>Obrigada por clicar!</strong>";
+    //pegando o elemento e o inner vai mesclar os dois
+
 }
 
-function validaIdade(idade)
-{
-    var validar;
-    if(idade > 18){
-        validar=true;
-    } else {
-        validar=false;
-    }
-    return validar;
+function direcionar(){
+    window.open("https://www.google.com");
+    //abre em uma nova janela o site passado no parametro
+    //esse abre em uma nova janela
+
+    window.location.href="https://www.google.com";
+    //outra funcao utilizada para abrir um link
+    //a diferença é que esse ultimo abre na mesma janela
 }
 
-var idade = prompt("Qual sua idade?");
-console.log(validarIdade(idade));
+function trocar(elemento){
+    //alert("Trocar texto");
+    //ao passar o mouse em cima do elemento, vai chamar esse alert
+    //document.getElementById("mouseMove").innerHTML = "Obrigado por passar o mouse";
+    //troca o texto do elemento ao passar o mouse em cima
+    elemento.innerHTML = "Obrigado por passar o mouse";
 
-function setReplace(frase, valor, novoValor){
-    return frase.replace(valor, novoValor);
+    //utilizando parametro e no html o this, nao precisamos pegar por id, mas sim passar o proprio elemento como parametro
+    //facilita a implementacao aqui
 }
 
-alert(soma(5,10));
-alert(setReplace("Vai Japão", "Japão", "Brasil"));
+function voltar(elemento){
+    //document.getElementById("mouseMove").innerHTML = "Passe o mouse aqui";
+    //mensagem que vai mostrar quando eu tirar o mouse
+    elemento.innerHTML ="Passe o mouse aqui";
+}
+
+function load(){
+    alert("Página carregada");
+    //vai chamar essa funcao quando a página carregar pois colocamos no body
+}
+
+function change(elemento){
+    console.log(elemento.value);
+    //pegando o valor do elemento quando eu selecionar uma opção do select
+}
